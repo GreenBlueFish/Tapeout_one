961af353f6b7de10cef77cf74f46eaba9a9894c2
Rowan Leonard <rowan.leonard76@gmail.com>
2026-09-17 10:45:59 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -18,6 +18,11 @@ module tb ();
   reg rst_n;
   reg ena;
   wire [7:0] uo_out;
+  wire [7:0] ui_in;
+  wire [7:0] uio_in
+  wire [7:0] uio_out
+  wire [7:0] uio_oe
+   
 `ifdef GL_TEST
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
@@ -31,8 +36,12 @@ module tb ();
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-     
+
+      .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
