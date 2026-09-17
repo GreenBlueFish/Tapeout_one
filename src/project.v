f256b3e24c5dde6fa4ec982d32d32abbd74cce3d
Rowan Leonard <rowan.leonard76@gmail.com>
2026-09-17 10:11:21 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -30,6 +30,10 @@ module tt_um_counter (
 
     assign uo_out = ena ? counter : 8'hz;
 
-    wire [27:0] _unused = {ui_in, uio_in, uio_out, uio_oe};
+    //pointless logic, I want to keep the unused wires
+    assign uio_out = 8'hz;
+    assign uio_oe[7:1]  = 7'hz;
+    wire _unused = &{ui_in, uio_in};
+    assign uio_oe[0] = _unused_ok;
 
 endmodule
