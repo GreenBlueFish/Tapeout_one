715fe850da984e711a90e42081ff9fa8f252cda7
Rowan Leonard <rowan.leonard76@gmail.com>
2026-09-17 09:44:41 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,10 +12,10 @@ module tt_um_counter (
     output wire [7:0] uo_out,
 
     //unused
-    reg [7:0] ui_in,
-    reg [7:0] uio_in,
-    wire [7:0] uio_out,
-    wire [7:0] uio_oe
+    input reg [7:0] ui_in,
+    input reg [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe
 );
     
     reg [7:0] counter;
